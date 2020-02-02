875d53a2737431a32428b986c08ea4d70a802b8a
Francesco Burelli <francesco.burelli@gmail.com>
2020-02-02 17:29:18 +0100

    core improved

--- app/tests/todotxt.js
+++ app/tests/todotxt.js
@@ -3,7 +3,7 @@ const todotxt = new TodoTXT()
 const assert = require('assert')
 
 describe('todotxt core', function () {
-  it('only simple description', function () {
+  it('simple description', function () {
     const line = 'Buy some food'
     const expectedTask = {
       done: false,
@@ -16,6 +16,82 @@ describe('todotxt core', function () {
       extraMetadata: {}
     }
     const task = todotxt.parse(line)
-    assert.strictEqual(task, expectedTask)
+    assert.deepEqual(task, expectedTask)
+  })
+  it('completed task with simple description', function () {
+    const line = 'x Buy some food'
+    const expectedTask = {
+      done: true,
+      priority: null,
+      completionDate: null,
+      creationDate: null,
+      description: 'Buy some food',
+      projects: [],
+      contexts: [],
+      extraMetadata: {}
+    }
+    const task = todotxt.parse(line)
+    assert.deepEqual(task, expectedTask)
+  })
+
+  it('simple description starting with x (false completed)', function () {
+    const line = 'xBuy some food'
+    const expectedTask = {
+      done: false,
+      priority: null,
+      completionDate: null,
+      creationDate: null,
+      description: 'xBuy some food',
+      projects: [],
+      contexts: [],
+      extraMetadata: {}
+    }
+    const task = todotxt.parse(line)
+    assert.deepEqual(task, expectedTask)
+  })
+  it('priority and simple description', function () {
+    const line = '(A) Buy some food'
+    const expectedTask = {
+      done: false,
+      priority: '(A)',
+      completionDate: null,
+      creationDate: null,
+      description: 'Buy some food',
+      projects: [],
+      contexts: [],
+      extraMetadata: {}
+    }
+    const task = todotxt.parse(line)
+    assert.deepEqual(task, expectedTask)
+  })
+  it('wrong priority and simple description', function () {
+    const line = '(a) Buy some food'
+    const expectedTask = {
+      done: false,
+      priority: null,
+      completionDate: null,
+      creationDate: null,
+      description: '(a) Buy some food',
+      projects: [],
+      contexts: [],
+      extraMetadata: {}
+    }
+    const task = todotxt.parse(line)
+    assert.deepEqual(task, expectedTask)
+  })
+  it('completion date and simple description', function () {
+    const line = '2020-12-31 Buy some food'
+    const expectedTask = {
+      done: false,
+      priority: null,
+      completionDate: new Date('2020-12-31T00:00:00Z'),
+      creationDate: null,
+      description: 'Buy some food',
+      projects: [],
+      contexts: [],
+      extraMetadata: {}
+    }
+    const task = todotxt.parse(line)
+    assert.deepEqual(task, expectedTask)
   })
 })
--- app/todotxt.js
+++ app/todotxt.js
@@ -6,13 +6,24 @@ function TodoTXT () {
 TodoTXT.prototype.load = function load (filePath) {
 
 }
-
+function trimOrNull (text) {
+  if (text === undefined || text === null) {
+    return null
+  }
+  return text.trim()
+}
+function dateFromString (text) {
+  if (!text) {
+    return null
+  }
+  return new Date(`${text.trim()}T00:00:00Z`)
+}
 TodoTXT.prototype.parse = function parse (line) {
   const matches = /(x )?(\([A-Z]\) )?([0-9]{4}-[0-9]{2}-[0-9]{2} )?([0-9]{4}-[0-9]{2}-[0-9]{2} )?(.*)$/.exec(line)
   return {
     done: !!matches[1],
-    priority: matches[2] || null,
-    completionDate: matches[3] || null,
+    priority: trimOrNull(matches[2]),
+    completionDate: dateFromString(matches[3]),
     creationDate: matches[4] || null,
     description: matches[5],
     projects: [],
